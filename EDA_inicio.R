8c3846b04338ffa162aded1780d0ed6319673c11
Guilherme dos Santos <39782870+guilhermedossantos@users.noreply.github.com>
2018-11-04 15:20:51 -0200

    Add files via upload

---
+++ EDA_inicio.R
@@ -0,0 +1,71 @@
+## Análise Exploratória do Festival
+#load("festival_do_rio.RData")
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(plotly)
+
+#Carregando o dataset
+load("festival_tidy.RData")
+
+
+#### Países ####
+paises <- festival_do_rio$paises
+paises <- paises[!is.na(paises)]
+
+#Separando os países de cada filme
+str_split(paises, "/") %>%
+  unlist %>% str_trim  -> paises
+
+#Transformando em um dataset com frequencias
+as_tibble(paises) %>%
+  rename(pais = value) %>%
+  count(pais, sort = T) %>%
+  mutate(pais = reorder(pais,n)) -> paises
+  
+
+# Fazendo o gráfico
+g <- ggplot(data = paises, aes(x = pais, y = n)) +
+  geom_col() +
+  coord_flip() +
+  xlab(NULL) + 
+  ylab("Países") +
+  ggtitle("Frequências dos Países") 
+ggplotly(g)
+
+#### Duração ####
+tempos <- festival_do_rio %>% 
+  select(duracao, genero) %>% 
+  dplyr::filter(!is.na(duracao)) #achei que poderi confundir com o filter do plotly 
+
+#Histograma da duração
+ggplot(data = tempos) +
+  aes(x = duracao, y=..density..) +
+  geom_histogram(bins = 30, fill = "#0c4c8a") +
+  labs(title = "Histograma das Durações dos Filmes",
+       x = "Duração", y = NULL) +
+  theme_minimal() -> g
+ggplotly(g)
+
+#### Mostras com mais filmes ####
+mostras <- festival_do_rio %>%
+  select(mostra) %>%
+  filter(!is.na(mostra)) %>% 
+  count(mostra) %>%
+  mutate(mostra = reorder(mostra, n))
+
+g <- ggplot(data = mostras)+
+  aes(x = mostra, y = n) +
+  geom_col() +
+  xlab("Número de sessões") +
+  coord_flip() 
+ggplotly(g)
+  
+
+
+
+
+
+
+
+
